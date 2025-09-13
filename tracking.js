4b062eed012063f35c7a84afff72656cba5351e6
Mashal <mashallahsam@gmail.com>
2025-09-13 15:59:32 +0530

    Updated checkmarks resetting daily

--- tracking.js
+++ tracking.js
@@ -1,12 +1,24 @@
 let habit = ["typing", "hackerrank", "leetcode", "exercise", "duolingo"];
+const currentDate = new Date();
+let today = currentDate.toDateString();
+let savedDate = localStorage.getItem("savedDate");
+
+document.getElementById("currentDate").innerHTML = today;
+
+
+if (savedDate !== today){
+    habit.forEach(id => { localStorage.removeItem(id) });
+}
 
 habit.forEach(id => {
     let box = document.getElementById(id);
     let saved = localStorage.getItem(id);
-
+    
     if (saved != null) box.checked = JSON.parse(saved);
-
+    
     box.addEventListener("change", function() {
         localStorage.setItem(id, JSON.stringify(box.checked));
     });
 });
+
+localStorage.setItem("savedDate", today);
